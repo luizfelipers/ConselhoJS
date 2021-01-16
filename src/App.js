ba360e9da6b38ffb873fb336f778b42aa3cf3a76
LuizFelipe <luizfelipers19@gmail.com>
2021-01-16 16:54:22 -0300

    card addition

--- src/App.js
+++ src/App.js
@@ -1,12 +1,56 @@
-
+import React from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>App init</h1>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    advice:'',
+  };
+
+  componentDidMount(){
+   // console.log('Component Did really mount');
+    this.fetchAdvice()
+   
+  }
+
+  fetchAdvice = () => {
+    axios.get('https://api.adviceslip.com/advice')
+    .then((response)=> {
+const {advice} = response.data.slip;
+
+      console.log(advice)
+      this.setState({advice});
+    })
+    .catch((err)=>{
+      console.log(err)
+    });
+  }
+
+
+  render() {const {advice} = this.state;
+
+    return (
+
+
+      <div className="App">
+        <div className='titulo'>
+          <h1>App init</h1>
+        </div>
+        
+        <br></br>
+        <div className='card'>
+            <h2 className='heading'>{advice}</h2>
+       <button className='botao'>
+         Me dê conselho!</button> 
+        
+      </div>
+        </div>
+       
+    );
+  }
+
+
 }
 
 export default App;
